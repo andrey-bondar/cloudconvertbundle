988d4c2ae1a26f081ecd87fc23616e8462c88d6e
Dron <dron@syneforge.com>
2014-12-16 18:41:57 +0200

    Bundle Configuration

--- DependencyInjection/BestModulesCloudConvertExtension.php
+++ DependencyInjection/BestModulesCloudConvertExtension.php
@@ -7,11 +7,6 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-/**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
 class BestModulesCloudConvertExtension extends Extension
 {
     /**
@@ -21,6 +16,8 @@ class BestModulesCloudConvertExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+        
+        $container->setParameter('best_modules_cloud_convert.api_key', $config['api_key']);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');

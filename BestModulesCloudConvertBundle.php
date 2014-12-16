05db15886de09ff8bbae5f014e13b82df19f6fec
Dron <dron@syneforge.com>
2014-12-16 17:39:04 +0200

    first commit

---
+++ BestModulesCloudConvertBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BestModules\CloudConvertBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BestModulesCloudConvertBundle extends Bundle
+{
+}
---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BestModules\CloudConvertBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('BestModulesCloudConvertBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
